01ab0d799d05e5918dd9e379fd1f199a9097dac8
Daniel Hutmacher <daniel@strd.co>
2024-02-26 13:59:27 +0100

    ¯\_(ツ)_/¯

--- sp_ctrl3.sql
+++ sp_ctrl3.sql
@@ -36,7 +36,7 @@ SHORTCUT:   In SQL Server Management Studio, go to Tools -> Options
             schema (with a dot) need to be enclosed in quotes for this
             to work in older versions of SSMS.
 
-VERSION:    2023-02-24
+VERSION:    2024-02-24
 
 */
 
